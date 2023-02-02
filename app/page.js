72482568d4fdd380407c7604bd3c0fcb68b274b0
awais <awaishassan1997@gmail.com>
2023-02-02 20:44:09 +0500

    About section and Added gradiants

--- app/page.js
+++ app/page.js
@@ -21,14 +21,14 @@ const Page = () => (
     </div>
     <div className="relative">
       <GetStarted />
-      <div className="gradient-03 z-0" />
+      <div className="gradient-04 z-0" />
 
       <WhatsNew />
     </div>
     <div className="relative">
       <World />
       <Insights />
-      <div className="gradient-03 z-0" />
+      <div className="gradient-04 z-0" />
 
       <Feedback />
     </div>
